c3ebf5a0f54b6cf4acfed3f92f4a289d58bc2886
jitka.maresova <jitka.maresova@dnai.ai>
2022-07-17 10:04:43 +0200

    login tests

--- cypress/integration/Login.feature
+++ cypress/integration/Login.feature
@@ -2,3 +2,17 @@ Feature: Login
 Scenario: Opening application
   Given I open the application
   Then I can see navbar with logo
+
+Scenario: Succesful login
+  Given I open the application
+  And I am logged out
+  When Click login button
+  And Login as "jitka@seznam.cz"
+  Then I am logged in
+
+Scenario: Login failed
+  Given I open the application
+  And I am logged out
+  When Click login button
+  And Login as "jitka@seznam.cz" with wrong password
+  Then I am not logged in
--- cypress/integration/Login/Login.ts
+++ cypress/integration/Login/Login.ts
@@ -1,10 +1,47 @@
-import { Then, When } from "cypress-cucumber-preprocessor/steps";
+import { Then, When } from 'cypress-cucumber-preprocessor/steps'
 
-When("I open the application", () => {
-  cy.visit("/");
-});
+When('I open the application', () => {
+  cy.visit('/')
+})
 
-Then("I can see navbar with logo", () => {
-  cy.url().should("contain", "applifting-exercise.netlify.app");
-  cy.get("#navbar").should("be.visible");
-});
+Then('I can see navbar with logo', () => {
+  cy.url().should('contain', 'applifting-exercise.netlify.app')
+  cy.get('#navbar').should('be.visible')
+  cy.get('#logo').should('be.visible')
+})
+
+When('I am logged out', () => {
+  cy.get('#login').should('be.visible').should('contain', 'Log in')
+})
+
+When('Click login button', () => {
+  cy.get('#login').click()
+})
+
+const credentials = {
+  'jitka@seznam.cz': 'ahoj',
+}
+
+Then('Login as {string}', (username: string) => {
+  const password = credentials[username]
+  if (!password) {
+    throw new Error('Trying to login with unknown user.')
+  }
+  cy.get('#email').clear().type(username)
+  cy.get('#password').clear().type(password)
+  cy.get('#login-button').click()
+})
+
+Then('I am logged in', () => {
+  cy.get('#my-articles').should('be.visible').should('contain', 'My articles')
+})
+
+When('Login as {string} with wrong password', (username: string) => {
+  cy.get('#email').clear().type(username)
+  cy.get('#password').clear().type('wrong password')
+  cy.get('#login-button').click()
+})
+
+Then('I am not logged in', () => {
+  cy.get('#error').should('be.visible').should('contain', 'Password invalid')
+})
